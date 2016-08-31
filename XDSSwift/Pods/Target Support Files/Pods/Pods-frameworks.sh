80c329d6562198c2270a53564be478c91aee6264
xudusheng <597132100@qq.com>
2016-08-31 18:03:02 +0800

    no message

--- XDSSwift/Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ XDSSwift/Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -84,8 +84,12 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "Pods/ReactiveCocoa.framework"
+  install_framework "Pods/Result.framework"
   install_framework "Pods/SnapKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "Pods/ReactiveCocoa.framework"
+  install_framework "Pods/Result.framework"
   install_framework "Pods/SnapKit.framework"
 fi
