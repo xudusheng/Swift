919dcc27ff3ab14c37b7da648e2a430df896263c
xudusheng <597132100@qq.com>
2017-02-16 23:11:33 +0800

    no message

--- ReactNative/q2002_react_native/index.ios.js
+++ ReactNative/q2002_react_native/index.ios.js
@@ -10,4 +10,4 @@ import QRoot from './q2002/root';
 import PJSView from './inMod/p.jsView';
 import QScrollableTabView from './Demo'
 import PNewsPage from './x_news/p.news'
-AppRegistry.registerComponent('q2002_react_native', () => PNewsPage);
+AppRegistry.registerComponent('q2002_react_native', () => QRoot);
